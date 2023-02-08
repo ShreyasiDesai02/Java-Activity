f548256354a47e267ad2d6c8da110f4bacc620c5
ShreyasiDesai02 <shreyasidesai02@gamil.com>
2023-02-08 16:32:50 +0530

    message2

--- my/oddsum.java
+++ my/oddsum.java
@@ -4,7 +4,7 @@ public class oddsum {
 	
 	public static void main(String[] args) 
 	{
-		int number, i, oddSum = 0;
+		int number, oddSum = 0;
 		Scanner sh = new Scanner(System.in);
 		
 		System.out.println(" Please Enter any Number : ");
@@ -20,5 +20,8 @@ public class oddsum {
             }
 		}
 		System.out.println("\n The Sum of Odd Numbers upto " + "  =  " + oddSum);
+
+		
+			
 	}
 }

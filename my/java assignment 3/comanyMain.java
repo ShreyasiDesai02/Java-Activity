1b6f9bf85936a612a821cd9c61401dbc5111b420
ShreyasiDesai02 <shreyasidesai02@gamil.com>
2023-02-09 18:04:48 +0530

    master

---
+++ my/java assignment 3/comanyMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class comanyMain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Company Name:");
+        String  = s.nextLine();
+    }
+}

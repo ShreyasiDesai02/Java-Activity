16dcac797222e8b482332cf7c2971ff15ec3fabf
ShreyasiDesai02 <shreyasidesai02@gamil.com>
2023-02-20 09:41:02 +0530

    updates

--- my/Sample Test Assignment/Assignment1/Main.java
+++ my/Sample Test Assignment/Assignment1/Main.java
@@ -1,20 +1,16 @@
 package Assignment1;
 
+import java.time.LocalDate;
 import java.util.*;
-
+import Assignment1.Team;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the team name: ");
 		String Name = sc.nextLine();
-
-        Player player = new Player();
-		Team t = new Team(Name, null);
-
-        while(true){
 		System.out.println("1.Add Player\r\n "
 				+ "2.Delete Player\r\n "
 				+ "3.Display Players\r\n "
@@ -23,48 +19,38 @@ public class Main {
 		
 		System.out.println("Enter the choice:");
 		int ch = sc.nextInt(); sc.nextLine();
-
 		
+		Team t = new Team(name, dob, skill, no_match, runs, wickets, nationality, powerRating, Pname, playerList);
 		
-        switch (ch) { 
-        case1: 
-            System.out.println("Enter the details of player in CSV format: ");
-            String details = sc.nextLine();
-
-            player = Player.createPlayer(details);
-            if(player!=null){
-            t.addPlayerToTeam(player);
-            break;
-            }
-        case2:
-    
-            System.out.println("Enter the player name to be deleted: ");
-            String name = sc.nextLine();
-        
-            if(t.removePlayerFromTeam(name)){
-            System.out.println("Player successfully deleted");
-            }else {
-            System.out.println("Player not found in the team");
-            }
-            break;
-    
-    
-        case3:
-            System.out.println("Player in : "+ Name );
-        
-            t.displayPlayers();
-            break;
-        
-    
-        case4:
-            return;
-    
-        default:
-            System.out.println("Invalid input");
-    }
-
-}    
-        
+		if(ch ==1) {
+			System.out.println("Enter the details of player in CSV format: ");
+			String details = sc.nextLine();
 
 
-}}
+			t.addPlayerToTeam();
+			
+		}
+		
+		else if(ch == 2) {
+			System.out.println("Enter the player name to be deleted: ");
+			String name = sc.nextLine();
+			
+			t.removePlayerFromTeam(name);
+		}
+		
+		else if(ch == 3) {
+			System.out.println("Player in : "+ Name );
+			
+			t.displayPlayers();
+		}
+		else if(ch == 4){
+			return 0;
+		}
+		else {
+			System.out.println("Inavlid Input");
+		}
+		
+
+}
+
+}
